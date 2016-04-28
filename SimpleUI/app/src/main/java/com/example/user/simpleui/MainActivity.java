4cff40abbe8d72b94500bf8998563ddb6b9e875d
vincent3c <vincent3c@gmail.com>
2016-04-28 19:40:01 +0800

    listview set on item click Listener

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.user.simpleui;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -13,6 +15,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -68,6 +71,18 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 RadioButton radioButton = (RadioButton) findViewById(checkedId);
                 drinkName = radioButton.getText().toString();
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // view = this.item.layout
+            // position第幾個item
+            // parent可取得Adapter
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //用parent取得OrderAdapter，再用OrderAdapter的getItem取得第幾筆資料，用position取得第幾筆。
+                Order order = (Order)parent.getAdapter().getItem(position);
+                Snackbar.make(view, order.note, Snackbar.LENGTH_SHORT).show();
             }
         });
 
